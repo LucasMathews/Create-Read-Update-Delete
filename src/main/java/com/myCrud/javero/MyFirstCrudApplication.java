014342951759695fb6189232c1448a39018bf530
LucasMathews <lucasmvg@hotmail.com>
2022-09-15 00:55:43 -0300

    MyFirstCRUD

---
+++ src/main/java/com/myCrud/javero/MyFirstCrudApplication.java
@@ -0,0 +1,14 @@
+package com.myCrud.javero;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstCrudApplication.class, args);
+	}
+
+}
